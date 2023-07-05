ec4662351147b2c829300bf2f0653a12eef7060b
JamillyHamid <hamidjamilly28@edu.unifil.br>
2023-07-05 19:48:44 -0300

    Criando migration

--- database/migrations/2023_06_27_221132_create_tasks_table.php
+++ database/migrations/2023_06_27_221132_create_tasks_table.php
@@ -4,25 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTasksTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string("descricao");
-            $table->timestamps();
-        });
-    }
+   public function up()
+   {
+       Schema::create('tasks', function (Blueprint $table) {
+           $table->id();
+           $table->string('title');
+           $table->text('description');
+           $table->boolean('status')->default(false);
+           $table->timestamps();
+       });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('tasks');
-    }
-};
+   public function down()
+   {
+       Schema::dropIfExists('tasks');
+   }
+}
